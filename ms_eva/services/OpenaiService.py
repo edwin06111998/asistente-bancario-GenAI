33a75a103cf2c735563c7af4e27b54fc8b929dd8
Edwin Veloz <edwin06111998@gmail.com>
2025-04-20 23:39:28 -0500

    Se agrega temporalmente APIKEY de OpenAI

--- ms_eva/services/OpenaiService.py
+++ ms_eva/services/OpenaiService.py
@@ -5,7 +5,8 @@ from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 
-API_KEY_OPENAI = os.environ['OPENAI_APIKEY']
+# API_KEY_OPENAI = os.environ['OPENAI_APIKEY']
+API_KEY_OPENAI = "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"
 
 def getOpenaiLlmInstance():
     return ChatOpenAI(
